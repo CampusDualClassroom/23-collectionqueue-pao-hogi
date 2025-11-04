f82b5ee118db51f0b044d58bb9466fc9dd63d8e1
Paola Holguin Giraldo <pao.hogi@gmail.com>
2025-11-04 16:37:49 +0100

    Solved Exercise 23

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -5,14 +5,33 @@ import java.util.Queue;
 
 public class Exercise {
     public static Queue<String> createQueue() {
+    Queue<String> queue = new LinkedList<>();
+
+    queue.add("Smith");
+    queue.add("Montessori");
+    queue.add("Peralta");
+    queue.add("House");
+
+    return queue;
 
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+    if (queue == null || queue.isEmpty()){
+        System.out.println("La cola está vacía.");
+        return;
+    }
+        System.out.println("Elemento en la cima de la cola (peek): " + queue.peek());
+        System.out.println("=============");
 
+        while (!queue.isEmpty()){
+            String element = queue.poll();
+            System.out.println("Eliminando elemento: " + element);
+        }
     }
 
     public static void main(String[] args) {
-
+    Queue<String> myQueue = createQueue();
+    printAndEmptyQueue(myQueue);
     }
 
 }
